320ff4d5e909490db4220cca5c3be7bd0b040048
esgonzal <71908476+esgonzal@users.noreply.github.com>
2024-01-22 17:51:18 -0300

    Se actualiza pantalla de login

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,27 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  cardNumber: string = '';
-
-  constructor() { }
-
-  ngOnInit(): void {
-    // Event listener for card reading
-    document.addEventListener('keydown', (event) => {
-      this.cardNumber += event.key;
-      if (event.key === 'Enter') {
-        const trimmedCardNumber = this.cardNumber.replace(/\D/g, '');
-        if (trimmedCardNumber !== '') {
-          console.log('Card Number:', trimmedCardNumber);
-          this.cardNumber = '';
-        }
-      }
-    });
-  }
+export class HomeComponent {
+  
 }
 
