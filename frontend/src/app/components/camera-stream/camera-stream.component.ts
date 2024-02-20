32da953fe13265ddd22c9e2c86ff21bc97a126ca
esgonzal <esteban.gonzalez.m@mail.pucv.cl>
2024-02-20 09:29:12 -0300

    Se agrega el frontend de la vista de camara

--- frontend/src/app/components/camera-stream/camera-stream.component.ts
+++ frontend/src/app/components/camera-stream/camera-stream.component.ts
@@ -1,40 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { CameraService } from '../../services/camera.service';
-import { cameraFeedResponse } from 'src/app/Interfaces/API_responses';
-import { lastValueFrom } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import Hls from 'hls.js';
 
 @Component({
   selector: 'app-camera-stream',
   templateUrl: './camera-stream.component.html',
   styleUrls: ['./camera-stream.component.css']
 })
-export class CameraStreamComponent implements OnInit {
+export class CameraStreamComponent implements AfterViewInit, OnInit {
+  @ViewChild('videoPlayer') videoPlayerRef: ElementRef<HTMLVideoElement>;
+  videoPlayer: HTMLVideoElement;
+  subscription: Subscription;
+  isStreaming: boolean = false;
 
-  streamUrl: string;
-
-  constructor(private cameraService: CameraService) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.startLiveStream();
+    this.startFfmpegProcess();
   }
 
-  async startLiveStream(): Promise<void> {
-    const ipAddress = 'your-camera-ip';
-    const devIndex = 'your-camera-dev-index';
-    let response = await lastValueFrom(this.cameraService.startLiveStream(ipAddress, devIndex)) as cameraFeedResponse;
-    if (response.success){
-      this.streamUrl = "ws://api.vohkapp.com:9999";
-    } else {
-      console.error('Error starting live stream:', response.error);
-    }
+  ngAfterViewInit(): void {
+    this.videoPlayer = this.videoPlayerRef.nativeElement;
+    this.setupVideoPlayer();
   }
 
-  async stopLiveStream(): Promise<void> {
-    let response = await lastValueFrom(this.cameraService.stopLiveStream()) as cameraFeedResponse;
-    if (response.success) {
-      console.log("stream detenido correctamente");
-    } else {
-      console.error('Error stopping live stream:', response.error);
+  startFfmpegProcess(): void {
+    this.http.get('http://localhost:3000/start-ffmpeg').subscribe(
+      () => {
+        console.log('FFmpeg process started successfully');
+      },
+      (error) => {
+        console.error('Error starting FFmpeg process:', error);
+      }
+    );
+  }
+
+  setupVideoPlayer(): void {
+    const videoSrc = 'http://localhost:3000/stream/index.m3u8';
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(this.videoPlayer);
+    } else if (this.videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
+      this.videoPlayer.src = videoSrc;
     }
   }
 }
