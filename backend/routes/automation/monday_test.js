7f3af27cfbb020ab3a48cb7300e2c9f6bf48c6d3
esgonzal <71908476+esgonzal@users.noreply.github.com>
2024-09-27 11:01:43 -0300

    se agrega url de prueba para integrar en monday

--- backend/routes/automation/monday_test.js
+++ backend/routes/automation/monday_test.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
+// Define tu token de API de Monday.com
+const MONDAY_API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxMTQ2Njk1OSwiYWFpIjoxMSwidWlkIjo2NjIwMTYyNSwiaWFkIjoiMjAyNC0wOS0xNlQyMDoyMDozNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU0OTM0MzksInJnbiI6InVzZTEifQ.fNmvWweIy7JWcaOWhggW_HbIc_hJk6weiuM_-HjGys8';
+
+async function obtenerDatosElemento(boardId, pulseId) {
+    const query = `
+        query {
+            boards(ids: ${boardId}) {
+                items(ids: ${pulseId}) {
+                    name
+                    column_values {
+                        id
+                        text
+                    }
+                }
+            }
+        }
+    `;
+
+    try {
+        const response = await axios.post(
+            'https://api.monday.com/v2', { query }, {
+                headers: {
+                    Authorization: MONDAY_API_TOKEN,
+                },
+            }
+        );
+
+        const itemData = response.data.data.boards[0].items[0];
+        return itemData;
+    } catch (error) {
+        console.error('Error obteniendo datos del elemento:', error);
+    }
+}
+
 // Ruta para procesar el webhook desde Monday.com
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
     // Obtener los datos que llegan del webhook de Monday.com
     const data = req.body;
 
     // Imprimir los datos para asegurarse de que están llegando correctamente
     console.log('Datos recibidos del webhook:', data);
+    // Obtén los ids del board y del elemento (pulse)
+    const boardId = data.event.boardId;
+    const pulseId = data.event.pulseId;
+
+    try {
+        const itemData = await obtenerDatosElemento(boardId, pulseId);
+        console.log(itemData);
+
+    } catch (error) {
+        console.error('Error procesando la solicitud:', error);
+        res.status(500).send('Error procesando la solicitud');
+    }
 
 });
 
