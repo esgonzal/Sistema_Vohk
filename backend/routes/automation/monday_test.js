5e3fa3da57fe2d473a96c879ee544aa736f8d6d5
esgonzal <71908476+esgonzal@users.noreply.github.com>
2024-09-27 11:14:55 -0300

    se agrega url de prueba para integrar en monday

--- backend/routes/automation/monday_test.js
+++ backend/routes/automation/monday_test.js
@@ -5,16 +5,14 @@ const router = express.Router();
 // Define tu token de API de Monday.com
 const MONDAY_API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxMTQ2Njk1OSwiYWFpIjoxMSwidWlkIjo2NjIwMTYyNSwiaWFkIjoiMjAyNC0wOS0xNlQyMDoyMDozNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU0OTM0MzksInJnbiI6InVzZTEifQ.fNmvWweIy7JWcaOWhggW_HbIc_hJk6weiuM_-HjGys8';
 
-async function obtenerDatosElemento(boardId, pulseId) {
+async function obtenerDatosElemento(pulseId) {
     const query = `
         query {
-            boards(ids: ${boardId}) {
-                items(ids: ${pulseId}) {
-                    name
-                    column_values {
-                        id
-                        text
-                    }
+            items(ids: [${pulseId}]) {
+                name
+                column_values {
+                    id
+                    text
                 }
             }
         }
@@ -25,14 +23,18 @@ async function obtenerDatosElemento(boardId, pulseId) {
             'https://api.monday.com/v2', { query }, {
                 headers: {
                     Authorization: MONDAY_API_TOKEN,
+                    'Content-Type': 'application/json',
                 },
             }
         );
 
-        const itemData = response.data.data.boards[0].items[0];
+        // Imprimir la respuesta de la API para depuración
+        console.log('Respuesta de la API de Monday:', response.data);
+
+        const itemData = response.data.data.items[0]; // Cambia aquí para acceder al primer elemento
         return itemData;
     } catch (error) {
-        console.error('Error obteniendo datos del elemento:', error);
+        console.error('Error obteniendo datos del elemento:', error.response ? error.response.data : error.message);
     }
 }
 
@@ -44,11 +46,10 @@ router.post('/', async(req, res) => {
     // Imprimir los datos para asegurarse de que están llegando correctamente
     console.log('Datos recibidos del webhook:', data);
     // Obtén los ids del board y del elemento (pulse)
-    const boardId = data.event.boardId;
     const pulseId = data.event.pulseId;
 
     try {
-        const itemData = await obtenerDatosElemento(boardId, pulseId);
+        const itemData = await obtenerDatosElemento(pulseId);
         console.log(itemData);
 
     } catch (error) {
