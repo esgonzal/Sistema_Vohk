eb81a33430cd700571ebb33eb241a7eea2adae8c
esgonzal <71908476+esgonzal@users.noreply.github.com>
2024-09-27 10:50:26 -0300

    se agrega url de prueba para integrar en monday

--- backend/routes/automation/monday_test.js
+++ backend/routes/automation/monday_test.js
@@ -9,14 +9,6 @@ router.post('/', (req, res) => {
     // Imprimir los datos para asegurarse de que están llegando correctamente
     console.log('Datos recibidos del webhook:', data);
 
-    // Verificar si el webhook es un challenge y devolverlo en la respuesta
-    if (data.challenge) {
-        // Devolver el challenge recibido
-        res.status(200).send({ challenge: data.challenge });
-    } else {
-        // Si no es un challenge, puedes procesar otros datos del webhook aquí
-        res.status(200).send('Webhook procesado con éxito');
-    }
 });
 
 module.exports = router;
