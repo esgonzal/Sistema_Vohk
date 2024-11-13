cbd623039dc9dcd6cb912f918e864d84dee12f9e
esgonzal <71908476+esgonzal@users.noreply.github.com>
2024-11-13 09:38:33 -0300

    Se agrega las direcciones originales de las maquinas virtuales

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: ['https://www.vohkapp.com', 'https://vohkapp.com', /^http:\/\/localhost:\d+$/, 'https://34.176.111.121'],
+    origin: ['https://www.vohkapp.com', 'https://vohkapp.com', /^http:\/\/localhost:\d+$/, 'https://34.176.111.121:4200', 'http://34.176.111.121:4200'],
 };
 app.use(cors(corsOptions));
 
