3f517aa3aacb0b15142f12a769105289ba7317ab
esgonzal <esteban.gonzalez.m@mail.pucv.cl>
2026-05-29 11:15:12 -0400

    se arreglan path de jsons

--- backend/routes/vohk_app/index.js
+++ backend/routes/vohk_app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// Import all v1 routes
+
 router.use('/twilio', require('./twilio'));
 router.use('/intercom', require('./intercomAPI'));
 router.use('/events', require('./events'));
