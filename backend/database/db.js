486a5cec2519c7941210bd1abffdb02836e2e1bf
esgonzal <esteban.gonzalez.m@mail.pucv.cl>
2026-06-16 10:31:06 -0400

    conexion digital ocean managed database

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'vohk',
-    user: 'vohk_user',
-    password: 'vohk_password',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
